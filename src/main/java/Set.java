5b17427470a87b732ad4e42c1e435410bac5818a
Ian O'Brien <Ian.OBrian@husbanken.no>
2019-03-14 10:29:40 +0100

    added set definition

---
+++ src/main/java/Set.java
@@ -0,0 +1,18 @@
+public class Set<T extends Object> implements Collection<T> {
+
+    @Override
+    public void add(T element) {
+
+    }
+
+    @Override
+    public T remove(int index) {
+        return null;
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+    
+}
